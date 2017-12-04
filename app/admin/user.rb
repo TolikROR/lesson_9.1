f9a63391f09fd903350fc94492482347c2d247ba
Tolik <anatoliyboyko1995@gmail.com>
2017-12-05 01:38:57 +0200

    add

---
+++ app/admin/user.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register User do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :first_name
+    column :last_name
+    actions
+  end
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
