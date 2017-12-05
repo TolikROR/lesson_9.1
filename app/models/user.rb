fcfdb1a22a242844b045d16172bd160f637acb0d
Tolik <anatoliyboyko1995@gmail.com>
2017-12-05 20:32:15 +0200

    add if

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
+         :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      if auth.info.email == nil
+        user.email = 'default_emaul@gmail.com'
+      end
       user.password = Devise.friendly_token[0,20]
     end
   end
